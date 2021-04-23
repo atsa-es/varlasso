2c6c2e4e4ceb7fd475ac8e4d273d9a38b55cf359
Eric Ward <eric-ward@users.noreply.github.com>
2021-04-23 07:33:59 -0700

    update

---
+++ R/marlasso-package.R
@@ -0,0 +1,16 @@
+#' The 'marlasso' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name marlasso-package
+#' @aliases marlasso
+#' @useDynLib marlasso, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2020). RStan: the R interface to Stan. R package version 2.21.2. https://mc-stan.org
+#'
+NULL
